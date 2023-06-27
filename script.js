a4a06853aac006b76dd0cfe52a4b071b78233167
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-27 15:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function wait(delay){
+	return new Promise(resolve => {
+		setTimeout(()=>{
+		resolve('delayed Message')
+	})
+	});
+}
+async function displayMessageAfterDelay(delay) {
+  const message = await wait(delay);
+  document.getElementById('output').textContent = message;
+}
+
+displayMessageAfterDelay(3000);
