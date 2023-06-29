d5d53359f9ba2a69a27d88c0f412907e02680eeb
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-29 14:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,10 @@
-//your JS code here. If required.
+const btn = document.getElementById("btn");
+btn.addEventListener('click',delayfun);
+async function delayfun(){
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+	const output = document.getElementById("output");
 
-
-
-
-// Function to delay for a specified time
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+	await new Promise(resolve => setTimeout(resolve, delay))
+	output.innerText = text;
 }
-
-// Async function to display message after a delay
-async function displayMessage() {
-  const textInput = document.getElementById('text');
-  const delayInput = document.getElementById('delay');
-  const outputDiv = document.getElementById('output');
-
-  const message = textInput.value;
-  const delayTime = parseInt(delayInput.value, 10);
-
-  outputDiv.textContent = 'Waiting...';
-
-  // Delay for the specified time
-  await delay(delayTime);
-
-  // Display the message after the delay
-  outputDiv.textContent = message;
-}
-
-// Event listener for the button click
-const btn = document.getElementById('btn');
-btn.addEventListener('click', displayMessage);
-
-// function wait(){
-// 	return new Promise(resolve => {
-// 		setTimeout(()=>{
-// 		resolve('delayed Message')
-// 	},3000)
-// 	});
-// }
-// async function displayMessageAfterDelay() {
-//   const message = await wait(delay);
-//   document.getElementById('text').innerText = message;
-// }
-// displayMessageAfterDelay() 
